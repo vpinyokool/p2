9c60c357bad0b83967e120a3d28b78b2388b7a96
Q <vpinyokool@me.com>
2020-08-27 09:38:02 -0700

    bring back assets

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -6,20 +6,34 @@ var call = {
 
     firstInit: function() {
         // run everywhere
-
         Global.states();
-        Global.filter();
         Global.backButton();
         Global.modal();
         Global.bodyMovin();
+        Global.likeThisGrid();
 
     },
     init: function() {
         // getting location path
         var sPath = window.location.pathname;
-
+        console.log(sPath);
         // run on specific page
+        if ( sPath == '/tag-one' ) {
+            Global.filter();
+        }
 
+        if ( sPath == '/tag-two' ) {
+            Global.autoTags();
+        }
+
+        if ( sPath == '/tag-three' ) {
+            Global.reactionGrid();
+            Global.reactionAnimation();
+        }
+
+        if ( sPath == '/tag-four' ) {
+            Global.organicTagsGrid();
+        }
 
     }
 }
